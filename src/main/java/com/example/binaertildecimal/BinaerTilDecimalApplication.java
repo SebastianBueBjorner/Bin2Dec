ad7bde272095c69c045a0eb1fa41841146567f1d
SebastianBueBjorner <seba169m@stud.kea.dk>
2022-02-17 01:11:10 +0100

    done

---
+++ src/main/java/com/example/binaertildecimal/BinaerTilDecimalApplication.java
@@ -0,0 +1,13 @@
+package com.example.binaertildecimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaerTilDecimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaerTilDecimalApplication.class, args);
+    }
+
+}
